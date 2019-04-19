1c114c50e40e3d2d3e7e539f4edd7a94a80f2c3a
George <mdnxpnsn@outlook.com>
2019-04-19 12:03:08 +0200

    fixing indentation

--- inc/BST.hpp
+++ inc/BST.hpp
@@ -11,33 +11,33 @@
 #include <cstdlib>
 
 typedef struct node {
-	int key;
-	double data;
-	node* left_child = NULL;
-	node* right_child = NULL;
-	node* parent = NULL;
+    int key;
+    double data;
+    node* left_child = NULL;
+    node* right_child = NULL;
+    node* parent = NULL;
 } nd;
 
 class Tree {
 private:
-	nd* root;
-	int size;
+    nd* root;
+    int size;
 
-	nd* subtree_minimum(nd* node);
-	nd* subtree_maximum(nd* node);
-	nd* tree_minimum();
-	nd* tree_maximum();
-	nd* successor(nd* node);
-	nd* predecessor(nd* node);
+    nd* subtree_minimum(nd* node);
+    nd* subtree_maximum(nd* node);
+    nd* tree_minimum();
+    nd* tree_maximum();
+    nd* successor(nd* node);
+    nd* predecessor(nd* node);
 public:
-	Tree();
-	void insert_node(nd node);
-	void delete_node(nd* node);
-	void delete_node_wrapper(int key);
-	nd* search_node(int key);
-	void print_tree();
-	void print_tree_reverse();
-	bool verify_bst_invariant();
+    Tree();
+    void insert_node(nd node);
+    void delete_node(nd* node);
+    void delete_node_wrapper(int key);
+    nd* search_node(int key);
+    void print_tree();
+    void print_tree_reverse();
+    bool verify_bst_invariant();
 };
 
 #endif /* BST_HPP_ */
